d7db5c26428126de6a6380cd24b34b0e3e76cda6
unknown <gjbonner@gmail.com>
2019-10-17 13:17:10 -0400

    add rest of alphabet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function coupon(num, codes, dash){
     const numeric = [0,1,2,3,4,5,6,7,8,9]
-    const alpha = ['A','B','C','D','E','F','G','H','I']
+    const alpha = ['A','B','C','D','E','F','G','H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     let codesArr = []
     let code = []
     let j = 0;
